d92900c6ace67ad715ca94afd4358b7462325f73
wu <jianhengwu0407@gmail.com>
2023-03-29 14:45:58 +0800

    initial commit

---
+++ docs/.vuepress/config.js
@@ -0,0 +1,22 @@
+module.exports = {
+    base: '/automated_api_pytest/docs/',
+    dest: 'public',
+    locales: {
+        '/': {
+            lang: 'zh-CN',
+            title: 'Automated Api Pytest',
+            description: 'docs for automated_api_pytest',
+        }
+    },
+    themeConfig: {
+        nav: [
+            {text: 'Home', link: '/'},
+            {text: 'Github', link: 'https://github.com/wu-clan'},
+            {text: 'Gitee', link: 'https://gitee.com/wu_cl'},
+        ],
+        sidebar: 'auto',
+        search: false,
+        searchMaxSuggestions: 10,
+        lastUpdated: 'Releases',
+    }
+}
